f850bbe8c432c9c76497678262f7740082459197
soloimsad <javier.gomeza@usm.cl>
2023-11-07 23:49:48 -0300

    buenas

--- welcome.php
+++ welcome.php
@@ -1,3 +1,33 @@
 <?php
-    include "barra.html"
+    session_start();
+    include 'basic.html'; 
+    include 'connection.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Minuta USM</title>
+</head>
+<body>
+    <form action = "" method = "get">
+        <input type ="text" name= "busqueda"> <br>
+        <input type ="submit" name= "enviar" value ="Buscar">
+    </form>
+    
+    <br><br>
+    <?php 
+    if(isset($_GET['enviar'])){
+        $busqueda=$_GET['busqueda'];
+        $consulta=$conn->query("SELECT * FROM recetas WHERE nombre LIKE '%$busqueda%'");
+        while ($row = $consulta->fetch_array()) {
+            echo $row['nombre'].'<br>';
+        }
+    }
+    ?>
+
+
+</body>
+</html>
